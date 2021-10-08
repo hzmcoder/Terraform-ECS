ce0f91526fcb6c07fc352aeaa3763c75d4aa804c
hzmcoder <92132850+hzmcoder@users.noreply.github.com>
2021-10-08 14:26:08 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,6 @@
-provider "alicloud" {
-  configuration_source = "Terradorm-ECS"
-}
-
 resource "alicloud_vpc" "main" {
   # VPC名称
   name       = "alicloud"
   # VPC地址块
   cidr_block = "10.1.0.0/21"
-}
-
-resource "alicloud_vswitch" "main" {
-  # VPC ID
-  vpc_id            = alicloud_vpc.main.id
-  # 交换机地址块
-  cidr_block        = "10.1.0.0/24"
-  # 可用区
-  availability_zone = "cn-hangzhou-b"
-  # 资源依赖,会优先创建该依赖资源
-  depends_on = [alicloud_vpc.main]
-}
-resource "alicloud_nat_gateway" "main" {
-  vpc_id        = alicloud_vpc.main.id
-  specification = "Small"
-  name          = "from-tf-example"
-}
-
-resource "alicloud_eip" "foo" {
-}
-
-resource "alicloud_eip_association" "foo" {
-  allocation_id = alicloud_eip.foo.id
-  instance_id   = alicloud_nat_gateway.main.id
 }
